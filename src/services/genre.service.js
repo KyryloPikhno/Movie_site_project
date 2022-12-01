5d9028b5b195c4e4599b2a63523d2604223167d3
KyryloPikhno <Mr.Good@i.ua>
2022-12-02 01:54:27 +0200

    Final commit

--- src/services/genre.service.js
+++ src/services/genre.service.js
@@ -4,6 +4,7 @@ import {urls} from "../configs";
 
 const genreService ={
     getAll:()=>axiosService.get(urls.genres),
+
     getById:(id, currentPage)=>axiosService.get(`${urls.movies}${urls.genresById}${id}&page=2${currentPage}`)
 }
 
